73e04fdc6d4da1cebe952ba4a992c7e24b766d45
Ziji <Zijipia@gmail.com>
2025-01-21 01:19:35 +0700

    hymmm

--- app/page.js
+++ app/page.js
@@ -7,8 +7,8 @@ export default function Home() {
 	return (
 		<div className='container mx-auto p-4'>
 			<h1 className='text-3xl font-bold mb-4'>Discord Music Bot Controller</h1>
-			<LoginButton />
-			<MusicController />
+			{/* <LoginButton />
+			<MusicController /> */}
 		</div>
 	);
 }
